a1a19b46fd97610d9909014c24cf6a3a4779e808
exafmm <devnull@localhost>
2011-03-05 05:01:20 +0900

    Added check_gpus.cxx and cuprintf.

--- unit_test/check_gpus.cxx
+++ unit_test/check_gpus.cxx
@@ -1,26 +1,32 @@
+#include "mympi.h"
 #include "dataset.h"
 #include "evaluator.h"
 
 int main() {
+  char hostname[256];
   double tic,toc;
-  const int numBodies = 100;
+  const int numBodies = 10000;
   tic = get_time();
   Bodies bodies(numBodies);
   Dataset D;
   Evaluator E;
+  MyMPI M;
   E.initialize();
+  gethostname(hostname,sizeof(hostname));
+  bool print = true;
+  if( M.commRank() != 0 ) print = false;
   toc = get_time();
-  std::cout << "Allocate      : " << toc-tic << std::endl;
+  if(print) std::cout << "Allocate      : " << toc-tic << std::endl;
 
   tic = get_time();
   D.sphere(bodies);
   toc = get_time();
-  std::cout << "Set bodies    : " << toc-tic << std::endl;
+  if(print) std::cout << "Set bodies    : " << toc-tic << std::endl;
 
   tic = get_time();
   E.evalP2P(bodies,bodies);
   toc = get_time();
-  std::cout << "Direct GPU    : " << toc-tic << std::endl;
+  if(print) std::cout << "Direct GPU    : " << toc-tic << std::endl;
 
   tic = get_time();
   real err = 0, rel = 0;
@@ -37,7 +43,16 @@ int main() {
     rel += pot * pot;
   }
   toc = get_time();
-  std::cout << "Direct CPU    : " << toc-tic << std::endl;
-  std::cout << "Error         : " << std::sqrt(err/rel) << std::endl;
+  if(print) std::cout << "Direct CPU    : " << toc-tic << std::endl;
+
+  for( int irank=0; irank!=M.commSize(); ++irank ) {
+    MPI_Barrier(MPI_COMM_WORLD);
+    if( M.commRank() == irank ) {
+      std::cout << hostname << " @ rank : " << M.commRank() << " / " << M.commSize();
+      std::cout << " @ device : " << M.commRank()%GPUS << " / " << GPUS << std::endl;
+      std::cout << "Error         : " << std::sqrt(err/rel) << std::endl;
+    }
+    usleep(100);
+  }
   E.finalize();
 }
