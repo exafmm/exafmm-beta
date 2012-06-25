bef575ee98a32a64a1685df02a5e510ae9f6b8eb
Rio Yokota <rioyokota@me.com>
2012-06-25 13:32:04 -0400

    Bugfix for GPU version. types.h set for GPU runs.

--- include/types.h
+++ include/types.h
@@ -83,8 +83,8 @@ extern int PAPIEVENT;                                           //!< PAPI event 
 #endif
 #endif
 
-const int  P        = 3;                                        //!< Order of expansions
-const int  NCRIT    = 10;                                       //!< Number of bodies per cell
+const int  P        = 10;                                       //!< Order of expansions
+const int  NCRIT    = 1000;                                     //!< Number of bodies per cell
 const int  MAXBODY  = 200000;                                   //!< Maximum number of bodies per GPU kernel
 const int  MAXCELL  = 10000000;                                 //!< Maximum number of bodies/coefs in cell per GPU kernel
 const real CLET     = 2;                                        //!< LET opening critetia
