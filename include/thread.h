7cf95804341e3aea3894e3c67c4b09f6ae1867c9
Rio Yokota <rioyokota@me.com>
2013-01-12 17:26:02 +0300

    Added OpenMP macros by Miquel.

--- include/thread.h
+++ include/thread.h
@@ -4,18 +4,7 @@
 // Thread model
 #if TBB
 #include <tbb/task_group.h>
-#define TASKS 1
 using namespace tbb;
-
-#elif MTHREAD
-#include <mtbb/task_group.h>
-#define TASKS 1
-
-#endif
-
-// Task based threading macros
-#if TASKS
-
 #define __init_tasks__                task_group tg
 #define __sync_tasks__                tg.wait()
 #define spawn_task0(E)                tg.run([=] { E; })
@@ -23,8 +12,25 @@ using namespace tbb;
 #define spawn_task2(s0, s1, E)        tg.run([=,&s0,&s1] { E; })
 #define spawn_task0_if(x, E)          if (x) { tg.run([=] { E; }); } else { E; }
 
-#else
+#elif MTHREAD
+#include <mtbb/task_group.h>
+#define __init_tasks__                task_group tg
+#define __sync_tasks__                tg.wait()
+#define spawn_task0(E)                tg.run([=] { E; })
+#define spawn_task1(s0, E)            tg.run([=,&s0] { E; })
+#define spawn_task2(s0, s1, E)        tg.run([=,&s0,&s1] { E; })
+#define spawn_task0_if(x, E)          if (x) { tg.run([=] { E; }); } else { E; }
 
+#elif OPENMP
+#define DO_PRAGMA(x)                  _Pragma( #x )
+#define __init_tasks__                DO_PRAGMA(omp parallel) DO_PRAGMA(omp single)
+#define __sync_tasks__                DO_PRAGMA(omp taskwait)
+#define spawn_task0(E)                DO_PRAGMA(omp task) { E; }
+#define spawn_task1(s0, E)            DO_PRAGMA(omp task shared (s0)) { E; }  
+#define spawn_task2(s0, s1, E)        DO_PRAGMA(omp task shared(s0,s1)) { E; }
+#define spawn_task0_if(x, E)          if(x) { DO_PRAGMA(omp task) { E; } } else { E; }
+
+#else
 #define __init_tasks__
 #define __sync_tasks__
 #define spawn_task0(E)                E
