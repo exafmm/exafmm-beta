a9493f8bd83c18c92364f33991da3bbb2705417b
Rio Yokota <rioyokota@me.com>
2013-11-08 21:04:11 +0900

    Simplify functor interface.

--- include/boundbox.h
+++ include/boundbox.h
@@ -8,39 +8,33 @@ class BoundBox : public Logger {
  private:
   int nspawn;                                                   //!< Threshold of NBODY for spawning new threads
 
-  struct boundsRecursionCallable {
-    BoundBox * boundbox;
-    B_iter BiBegin; B_iter BiEnd; Bounds & bounds;
-  boundsRecursionCallable(BoundBox * boundbox_, B_iter BiBegin_, B_iter BiEnd_, Bounds & bounds_) :
-    boundbox(boundbox_), BiBegin(BiBegin_), BiEnd(BiEnd_), bounds(bounds_) {}
-    void operator() () { bounds = boundbox->boundsRecursion(BiBegin, BiEnd, bounds); }
+  //! Recursive functor for calculating bounds
+  struct boundsFunctor {
+    B_iter BiBegin; B_iter BiEnd; Bounds & bounds; int nspawn;  // State variables
+    boundsFunctor(B_iter _BiBegin, B_iter _BiEnd, Bounds & _bounds, int _nspawn) : // Constructor
+      BiBegin(_BiBegin), BiEnd(_BiEnd), bounds(_bounds), nspawn(_nspawn) {}// Initialize state variables
+    Bounds operator() () {                                      // Overload operator()
+      assert(BiEnd - BiBegin > 0);                              // Validate range
+      if (BiEnd - BiBegin < nspawn) {                           // If number of elements is small enough
+	for (B_iter B=BiBegin; B!=BiEnd; B++) {                 //  Loop over range of bodies
+	  bounds.Xmin = min(B->X, bounds.Xmin);                 //   Update Xmin
+	  bounds.Xmax = max(B->X, bounds.Xmax);                 //   Update Xmax
+	}                                                       //  End loop over range of bodies
+	return bounds;                                          //  Return Xmin and Xmax as pair
+      } else {                                                  // Else if number of elements are large
+	B_iter BiMid = BiBegin + (BiEnd - BiBegin) / 2;         //  Middle iterator
+	task_group;                                             //  Initialize tasks
+        boundsFunctor leftBounds(BiBegin, BiMid, bounds, nspawn);// Recursion for left branch
+	create_taskc(leftBounds);                               //  Create task for left branch
+        boundsFunctor rightBounds(BiMid, BiEnd, bounds, nspawn);// Recursion for right branch
+	Bounds bounds2 = rightBounds();                         //  Use same task for right branch
+	wait_tasks;                                             //  Synchronize tasks
+	bounds.Xmin = min(bounds.Xmin, bounds2.Xmin);           //  Minimum of the two Xmins
+	bounds.Xmax = max(bounds.Xmax, bounds2.Xmax);           //  Maximum of the two Xmaxs
+	return bounds;                                          //  Return Xmin and Xmax
+      }                                                         // End if for number fo elements
+    }
   };
-
-  boundsRecursionCallable
-    boundsRecursion_(B_iter BiBegin_, B_iter BiEnd_, Bounds & bounds_) {
-    return boundsRecursionCallable(this, BiBegin_, BiEnd_, bounds_);
-  }
-
-  //! Recursively get Xmin and Xmax of domain
-  Bounds boundsRecursion(B_iter BiBegin, B_iter BiEnd, Bounds bounds) {
-    assert(BiEnd - BiBegin > 0);
-    if (BiEnd - BiBegin < nspawn) {                             // If number of elements is small enough
-      for (B_iter B=BiBegin; B!=BiEnd; B++) {                   //  Loop over range of bodies
-        bounds.Xmin = min(B->X, bounds.Xmin);                   //   Update Xmin
-        bounds.Xmax = max(B->X, bounds.Xmax);                   //   Update Xmax
-      }                                                         //  End loop over range of bodies
-      return bounds;                                            //  Return Xmin and Xmax as pair
-    } else {                                                    // Else if number of elements are large
-      B_iter BiMid = BiBegin + (BiEnd - BiBegin) / 2;           //  Middle iterator
-      task_group;                                               //  Initialize tasks
-      create_taskc(boundsRecursion_(BiBegin, BiMid, bounds));
-      Bounds bounds2 = boundsRecursion(BiMid, BiEnd, bounds);   //  Recursive call with old task
-      wait_tasks;                                               //  Synchronize tasks
-      bounds.Xmin = min(bounds.Xmin, bounds2.Xmin);             //  Minimum of the two Xmins
-      bounds.Xmax = max(bounds.Xmax, bounds2.Xmax);             //  Maximum of the two Xmaxs
-      return bounds;                                            //  Return Xmin and Xmax
-    }                                                           // End if for number fo elements
-  }
 
  public:
   BoundBox(int _nspawn) : nspawn(_nspawn) {}
@@ -53,7 +47,8 @@ class BoundBox : public Logger {
       bounds.Xmin = bounds.Xmax = 0;                            //  Set bounds to 0
     } else {                                                    // If body vector is not empty
       bounds.Xmin = bounds.Xmax = bodies.front().X;             //  Initialize Xmin, Xmax
-      bounds = boundsRecursion(bodies.begin(),bodies.end(),bounds);// Recursive call for bounds calculation
+      boundsFunctor boundsRecursion(bodies.begin(),bodies.end(),bounds,nspawn);// Initialize recursion
+      bounds = boundsRecursion();                               // Recursive call for bounds calculation
     }                                                           // End if for empty body vector
     stopTimer("Get bounds");                                    // Stop timer
     return bounds;                                              // Return Xmin and Xmax
@@ -62,7 +57,8 @@ class BoundBox : public Logger {
   //! Update Xmin and Xmax of domain
   Bounds getBounds(Bodies bodies, Bounds bounds) {
     startTimer("Get bounds");                                   // Start timer
-    bounds = boundsRecursion(bodies.begin(),bodies.end(),bounds);// Recursive call for bounds calculation
+    boundsFunctor boundsRecursion(bodies.begin(),bodies.end(),bounds,nspawn);// Initialize recursion
+    bounds = boundsRecursion();                                 // Recursive call for bounds calculation
     stopTimer("Get bounds");                                    // Stop timer
     return bounds;                                              // Return Xmin and Xmax
   }
--- include/thread.h
+++ include/thread.h
@@ -14,17 +14,17 @@ typedef mtbb::task_group task_group_t;
 #endif
 
 #if TBB || MTHREAD || QTHREAD || NANOX
-#define task_group                    task_group_t __tg__
-#define wait_tasks                    __tg__.wait()
-#define create_taskc(E)               __tg__.run(E)
-#define create_taskc_if(x, E)         if (x) { create_taskc(E); } else { E(); }
-
-#else /* not TBB, MTHREAD, QTHREAD, or NANOX */
+#define task_group            task_group_t __tg__
+#define wait_tasks            __tg__.wait()
+#define create_taskc(E)       __tg__.run(E)
+#define create_taskc_if(x, E) if (x) { create_taskc(E); } else { E(); }
+#else /* not TBB || MTHREAD || QTHREAD || NANOX */
 #define task_group
 #define wait_tasks
-#define create_taskc(E)                E();
-#define create_taskc_if(x, E)          E();
-#endif
+#define create_taskc(E)       E();
+#define create_taskc_if(x, E) E();
+#endif /* TBB || MTHREAD || QTHREAD || NANOX */
+
 #endif /* COMMON_CLIKH */
 
 #endif
