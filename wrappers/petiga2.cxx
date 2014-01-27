c920c959b6180b8591c2acde70c28a944f3186c2
Rio Yokota <rioyokota@me.com>
2014-01-27 11:34:12 +0300

    Debugging parallel petiga.cxx: Step. 8

--- wrappers/petiga2.cxx
+++ wrappers/petiga2.cxx
@@ -53,6 +53,71 @@ extern "C" void FMM_Finalize() {
   delete treeMPI;
 }
 
+void MPI_Shift(double * var, int &nold, int mpisize, int mpirank) {
+  const int isend = (mpirank + 1          ) % mpisize;
+  const int irecv = (mpirank - 1 + mpisize) % mpisize;
+  int nnew;
+  MPI_Request sreq, rreq;
+  MPI_Isend(&nold, 1, MPI_DOUBLE, irecv, 0, MPI_COMM_WORLD, &sreq);
+  MPI_Irecv(&nnew, 1, MPI_DOUBLE, isend, 0, MPI_COMM_WORLD, &rreq);
+  MPI_Wait(&sreq, MPI_STATUS_IGNORE);
+  MPI_Wait(&rreq, MPI_STATUS_IGNORE);
+  double * buf = new double [nnew];
+  MPI_Isend(var, nold, MPI_DOUBLE, irecv, 1, MPI_COMM_WORLD, &sreq);
+  MPI_Irecv(buf, nnew, MPI_DOUBLE, isend, 1, MPI_COMM_WORLD, &rreq);
+  MPI_Wait(&sreq, MPI_STATUS_IGNORE);
+  MPI_Wait(&rreq, MPI_STATUS_IGNORE);
+  for (int i=0; i<nnew; i++) {
+    var[i] = buf[i];
+  }
+  nold = nnew;
+  delete[] buf;
+}
+
+extern "C" void Direct_Laplace(int ni, double * xi, double * yi, double * zi, double * vi,
+                               int nj, double * xj, double * yj, double * zj, double * vj) {
+  const int Nmax = 2000000;
+  double * x2 = new double [Nmax];
+  double * y2 = new double [Nmax];
+  double * z2 = new double [Nmax];
+  double * v2 = new double [Nmax];
+  for (int i=0; i<nj; i++) {
+    x2[i] = xj[i];
+    y2[i] = yj[i];
+    z2[i] = zj[i];
+    v2[i] = vj[i];
+  }
+  int n2 = nj;
+  if (treeMPI->mpirank == 0) std::cout << "--- MPI direct sum ---------------" << std::endl;
+  for (int irank=0; irank<treeMPI->mpisize; irank++) {
+    if (treeMPI->mpirank == 0) std::cout << "Direct loop          : " << irank+1 << "/" << treeMPI->mpisize << std::endl;
+    MPI_Shift(x2, nj, treeMPI->mpisize, treeMPI->mpirank);
+    nj = n2;
+    MPI_Shift(y2, nj, treeMPI->mpisize, treeMPI->mpirank);
+    nj = n2;
+    MPI_Shift(z2, nj, treeMPI->mpisize, treeMPI->mpirank);
+    nj = n2;
+    MPI_Shift(v2, nj, treeMPI->mpisize, treeMPI->mpirank);
+    for (int i=0; i<ni; i++) {
+      double pp = 0;
+      for (int j=0; j<nj; j++) {
+        double dx = xi[i] - x2[j];
+        double dy = yi[i] - y2[j];
+        double dz = zi[i] - z2[j];
+        double R2 = dx * dx + dy * dy + dz * dz;
+        double invR = 1 / std::sqrt(R2);
+        if (R2 == 0) invR = 0;
+        pp += v2[j] * invR;
+      }
+      vi[i] += pp;
+    }
+  }
+  delete[] x2;
+  delete[] y2;
+  delete[] z2;
+  delete[] v2;
+}
+
 int main(int argc, char ** argv) {
   Dataset data;
   Verify verify;
@@ -97,9 +162,30 @@ int main(int argc, char ** argv) {
   logger->printTitle("FMM Profiling");
   logger->startTimer("Total FMM");
   logger->startPAPI();
-  Bodies bodies = data.initBodies(args->numBodies, args->distribution, treeMPI->mpirank, treeMPI->mpisize);
+  Bodies bodies(ni);
+  for (B_iter B=bodies.begin(); B!=bodies.end(); B++) {
+    int i = B-bodies.begin();
+    B->X[0]   = xi[i];
+    B->X[1]   = yi[i];
+    B->X[2]   = zi[i];
+    B->SRC    = 1;
+    B->TRG[0] = vi[i];
+    B->TRG[1] = 0;
+    B->TRG[2] = 0;
+    B->TRG[3] = 0;
+    B->IBODY = i;
+  }
+  Bodies jbodies(nj);
+  for (B_iter B=jbodies.begin(); B!=jbodies.end(); B++) {
+    int i = B-jbodies.begin();
+    B->X[0]   = xj[i];
+    B->X[1]   = yj[i];
+    B->X[2]   = zj[i];
+    B->SRC    = vj[i];
+    B->TRG    = 0;
+    B->IBODY = i;
+  }
   Bounds localBounds = boundbox->getBounds(bodies);
-  Bodies jbodies = data.initBodies(args->numBodies, args->distribution, treeMPI->mpirank+treeMPI->mpisize, treeMPI->mpisize);
   localBounds = boundbox->getBounds(jbodies,localBounds);
   Bounds globalBounds = treeMPI->allreduceBounds(localBounds);
   localBounds = treeMPI->partition(bodies,globalBounds);
@@ -157,6 +243,36 @@ int main(int argc, char ** argv) {
   build->printTreeData(cells);
   traversal->printTraversalData();
   logger->printPAPI();
+
+  B_iter B2=bodies2.begin();
+  for (B_iter B=bodies.begin(); B!=bodies.end(); B++,B2++) {
+    int i = B->IBODY;
+    xi[i] = B->X[0];
+    yi[i] = B->X[1];
+    zi[i] = B->X[2];
+    vi[i] = B->TRG[0];
+    std::cout << B-bodies.begin() << " " << B->TRG[0] << " " << B2->TRG[0] << std::endl;
+  }
+  ni = 100;
+  for (int i=0; i<ni; i++) {
+    v2[i] = 0;
+  }
+  Direct_Laplace(ni, xi, yi, zi, v2, nj, xj, yj, zj, vj);
+  potDif = potNrm = 0;
+  for (int i=0; i<ni; i++) {
+    potDif += (vi[i] - v2[i]) * (vi[i] - v2[i]);
+    potNrm += v2[i] * v2[i];
+  }
+  //double potDifGlob, potNrmGlob;
+  MPI_Reduce(&potDif, &potDifGlob, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+  MPI_Reduce(&potNrm, &potNrmGlob, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+  if (mpirank == 0) {
+    std::cout << "--- FMM vs. Direct ---------------" << std::endl;
+    std::cout << std::setw(stringLength) << std::left << std::scientific
+              << "Rel. L2 Error (pot)" << " : " << std::sqrt(potDifGlob/potNrmGlob) << std::endl;
+  }
+
+
   delete[] xi;
   delete[] yi;
   delete[] zi;
