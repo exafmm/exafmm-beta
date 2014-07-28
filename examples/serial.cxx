55b93a85d627c3d253ca2c79418f63d6deae32d1
Rio Yokota <rioyokota@me.com>
2014-07-28 20:18:41 +0300

    Makefile Cilk option.

--- examples/serial.cxx
+++ examples/serial.cxx
@@ -1,6 +1,10 @@
 #include "args.h"
 #include "bound_box.h"
+#ifdef CILK
 #include "build_tree3.h"
+#else
+#include "build_tree.h"
+#endif
 #include "dataset.h"
 #include "logger.h"
 #include "traversal.h"
