f0472b2246ed0a9e1335cc05cc52d5641a21325b
Kenjiro Taura <tau@logos.t.u-tokyo.ac.jp>
2014-09-08 12:32:09 +0900

    thread.h now works with OpenMP, TBB, MassiveThreads, and Qthreads with and without DAG_RECORDER

--- include/thread.h
+++ include/thread.h
@@ -2,42 +2,54 @@
 #define thread_h
 
 #if TBB
-#if DAG_RECORDER == 2
-#define TO_TBB 1
+#define num_threads(E)                tbb::task_scheduler_init init(E)
+#if DAG_RECORDER == 2  /* TBB with DAG Recorder */
+#define TO_TBB 1 
 #include <tpswitch/tpswitch.h>
-#else
+#include <tbb/task_scheduler_init.h>
+using namespace mtbb;
+#else  /* TBB without DAG Recorder; original TBB */
 #include <tbb/task_group.h>
 #include <tbb/task_scheduler_init.h>
 using namespace tbb;
-#endif
-
-#elif MTHREAD
-#define TO_MTHREAD_NATIVE 1
-#include <tpswitch/tpswitch.h>
-
-#elif QTHREAD
-#define TO_QTHREAD 1
-#include <tpswitch/tpswitch.h>
-
-#endif
-
-#if TO_TBB || TO_MTHREAD_NATIVE || TO_QTHREAD
-#define num_threads(E)
-
-#elif TBB
 #define mk_task_group                 task_group tg;
 #define wait_tasks                    tg.wait()
 #define create_taskc(E)               tg.run(E)
 #define create_taskc_if(x, E)         if(x) { create_taskc(E); } else { E(); }
-#define num_threads(E)                task_scheduler_init init(E);
+#endif
 
 #elif OPENMP
+#include <omp.h>
+#define num_threads(E)                omp_set_num_threads(E);
+#if DAG_RECORDER == 2		/* OpenMP with DAG Recorder */
+#define TO_OMP 1
+#include <tpswitch/tpswitch.h>
+#else  /* OpenMP without DAG Recorder; original OpenMP */
 #define PRAGMA_OMP(x)                 _Pragma( #x )
 #define mk_task_group
 #define wait_tasks                    PRAGMA_OMP(omp taskwait)
 #define create_taskc(E)               PRAGMA_OMP(omp task) E()
 #define create_taskc_if(x, E)         if(x) { create_taskc(E); } else { E(); }
-#define num_threads(E)                omp_set_num_threads(E);
+#endif
+
+#elif MTHREAD
+/* MassiveThreads (TBB-like interface on top of MassiveThreads)  */
+#define num_threads(E)		myth_init_withparam(E, 1 << 16)
+#define TO_MTHREAD_NATIVE 1
+#include <tpswitch/tpswitch.h>
+
+#elif QTHREAD
+/* Qthreads (TBB-like interface on top of MassiveThreads).
+   make sure you set QTHREAD_STACK_SIZE large enough (e.g., 131072) */
+#define num_threads(E)		do { char n[30]; sprintf(n, "%d", E); setenv("QTHREAD_STACK_SIZE", "65536", 0); setenv("QTHREAD_NUM_SHEPHERDS", n, 0); setenv("QTHREAD_NUM_WORKERS_PER_SHEPHERD", "1", 0); qthread_initialize(); } while(0)
+#define TO_QTHREAD 1
+#include <tpswitch/tpswitch.h>
+
+#elif SERIAL
+/* Qthreads (TBB-like interface on top of MassiveThreads)  */
+#define num_threads(E)
+#define TO_SERIAL 1
+#include <tpswitch/tpswitch.h>
 
 #else
 #define mk_task_group
