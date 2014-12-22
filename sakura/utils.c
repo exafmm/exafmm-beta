26f1c9950ffb9d0e52cf8b5c43089e8b734d921a
Rio Yokota <rioyokota@me.com>
2014-12-22 10:12:19 +0300

    Add utils.c

---
+++ sakura/utils.c
@@ -0,0 +1,17 @@
+#include "utils.h"
+
+void* sakura_malloc(size_t items, size_t size, char *message){
+  void *ptr = malloc(items*size);
+  if(ptr == 0){
+    printf("Out of memory at %s\n", message);
+  }
+  return ptr;
+}
+
+void* sakura_calloc(size_t items, size_t size, char* message){
+  void *ptr = calloc(items, size);
+  if(ptr == 0){
+    printf("Out of memory %s\n", message);
+  }
+  return ptr;
+}
