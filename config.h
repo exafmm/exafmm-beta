a48119c9d1c95d4a9fdd68cb38ec807137198422
Rio Yokota <rioyokota@gmail.com>
2015-09-24 10:02:34 +0900

    autoconf overhaul: Step 14.

---
+++ config.h
@@ -0,0 +1,56 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to compile in single precision. */
+#define EXAFMM_SINGLE 1
+
+/* Define to enable AVX optimizations. */
+/* #undef HAVE_AVX */
+
+/* Define if you have the MPI library. */
+/* #undef HAVE_MPI */
+
+/* Define to enable ARM NEON optimizations. */
+/* #undef HAVE_NEON */
+
+/* Define to enable SSE/SSE3 optimizations. */
+/* #undef HAVE_SSE3 */
+
+/* Name of package */
+#define PACKAGE "exafmm"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "exaFMM"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "exaFMM 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "exafmm"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Enable Kahan summation. */
+/* #undef USE_KAHAN */
+
+/* Disable SIMD optimizations. */
+/* #undef USE_SIMD */
+
+/* Enable weight for partitioning. */
+/* #undef USE_WEIGHT */
+
+/* Version number of package */
+#define VERSION "1.0"
+
+/* Use Intel Cilk */
+#define WITH_CILK 1
+
+/* Use Intel TBB */
+/* #undef WITH_TBB */
