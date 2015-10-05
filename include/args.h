f0f99b9ec017ddf0bbb9f15ac18b4ba0d8838b68
Rio Yokota <rioyokota@gmail.com>
2015-10-05 15:14:24 +0900

    args.h: Set [0/1] runtime arguments to run without value.

--- include/args.h
+++ include/args.h
@@ -1,3 +1,4 @@
+// Argument parser based on GNU getopt
 #ifndef args_h
 #define args_h
 #include <cstdio>
@@ -6,99 +7,102 @@
 #include <iostream>
 #include <iomanip>
 
+// In alphabetical order of the short option
 static struct option long_options[] = {
-  {"numBodies",    1, 0, 'n'},
-  {"ncrit",        1, 0, 'c'},
-  {"nspawn",       1, 0, 's'},
-  {"threads",      1, 0, 'T'},
-  {"images",       1, 0, 'i'},
-  {"theta",        1, 0, 't'},
-  {"dual",         1, 0, 'D'},
-  {"useRmax",      1, 0, 'x'},
-  {"useRopt",      1, 0, 'o'},
-  {"mutual",       1, 0, 'm'},
-  {"graft",        1, 0, 'g'},
-  {"verbose",      1, 0, 'v'},
-  {"distribution", 1, 0, 'd'},
-  {"repeat",       1, 0, 'r'},
-  {"IneJ",         1, 0, 'j'},
-  {"write",        1, 0, 'w'},
-  {"help",         0, 0, 'h'},
+  {"ncrit",        required_argument, 0, 'c'},
+  {"distribution", required_argument, 0, 'd'},
+  {"dual",         no_argument,       0, 'D'},
+  {"graft",        no_argument,       0, 'g'},
+  {"help",         no_argument,       0, 'h'},
+  {"images",       required_argument, 0, 'i'},
+  {"IneJ",         no_argument,       0, 'j'},
+  {"mutual",       no_argument,       0, 'm'},
+  {"numBodies",    required_argument, 0, 'n'},
+  {"useRopt",      no_argument,       0, 'o'},
+  {"repeat",       required_argument, 0, 'r'},
+  {"nspawn",       required_argument, 0, 's'},
+  {"theta",        required_argument, 0, 't'},
+  {"threads",      required_argument, 0, 'T'},
+  {"verbose",      no_argument,       0, 'v'},
+  {"write",        no_argument,       0, 'w'},
+  {"useRmax",      no_argument,       0, 'x'},
   {0, 0, 0, 0}
 };
 
 class Args {
 public:
-  int numBodies;
+  // In alphabetical order of the short option
   int ncrit;
-  int nspawn;
-  int threads;
-  int images;
-  double theta;
-  int dual;
-  int useRmax;
-  int useRopt;
-  int mutual;
-  int graft;
-  int verbose;
   const char * distribution;
-  int repeat;
+  int dual;
+  int graft;
+  int images;
   int IneJ;
+  int mutual;
+  int numBodies;
+  int useRopt;
+  int repeat;
+  int nspawn;
+  double theta;
+  int threads;
+  int verbose;
   int write;
+  int useRmax;
 
 private:
   void usage(char * name) {
     fprintf(stderr,
             "Usage: %s [options]\n"
             "Long option (short option)     : Description (Default value)\n"
-            " --numBodies (-n)              : Number of bodies (%d)\n"
+	    "In alphabetical order of the short option\n"
             " --ncrit (-c)                  : Number of bodies per leaf cell (%d)\n"
-            " --nspawn (-s)                 : Threshold for stopping task creation during recursion (%d)\n"
-            " --threads (-T)                : Number of threads (%d)\n"
-            " --images (-i)                 : Number of periodic image levels (%d)\n"
-            " --theta (-t)                  : Multipole acceptance criterion (%f)\n"
-	    " --dual (-D)                   : Use dual tree traversal (%d)\n"
-	    " --useRmax (-x) [0/1]          : Use maximum distance for MAC (%d)\n"
-	    " --useRopt (-o) [0/1]          : Use error optimized theta for MAC (%d)\n"
-            " --mutual (-m) [0/1]           : Use mutual interaction (%d)\n"
-	    " --graft (-g) [0/1]            : Graft remote trees to global tree (%d)\n"
-	    " --verbose (-v) [0/1]          : Print information to screen (%d)\n"
             " --distribution (-d) [l/c/s/p] : lattice, cube, sphere, octant, plummer (%s)\n"
-            " --repeat (-r)                 : Number of iteration loops (%d)\n"
+	    " --dual (-D)                   : Use dual tree traversal (%d)\n"
+	    " --graft (-g)                  : Graft remote trees to global tree (%d)\n"
+            " --help (-h)                   : Show this help document\n"
+            " --images (-i)                 : Number of periodic image levels (%d)\n"
             " --IneJ (-j)                   : Use different sources & targets (%d)\n"
+            " --mutual (-m)                 : Use mutual interaction (%d)\n"
+            " --numBodies (-n)              : Number of bodies (%d)\n"
+	    " --useRopt (-o)                : Use error optimized theta for MAC (%d)\n"
+            " --repeat (-r)                 : Number of iteration loops (%d)\n"
+            " --nspawn (-s)                 : Threshold for stopping task creation during recursion (%d)\n"
+            " --theta (-t)                  : Multipole acceptance criterion (%f)\n"
+            " --threads (-T)                : Number of threads (%d)\n"
+	    " --verbose (-v)                : Print information to screen (%d)\n"
             " --write (-w)                  : Write timings to file (%d)\n"
-            " --help (-h)                   : Show this help document\n",
+	    " --useRmax (-x)                : Use maximum distance for MAC (%d)\n",
             name,
-            numBodies,
             ncrit,
-            nspawn,
-	    threads,
-            images,
-            theta,
-	    dual,
-	    useRmax,
-	    useRopt,
-            mutual,
-	    graft,
-	    verbose,
             distribution,
-	    repeat,
+	    dual,
+	    graft,
+            images,
 	    IneJ,
-	    write);
+            mutual,
+            numBodies,
+	    useRopt,
+	    repeat,
+            nspawn,
+            theta,
+	    threads,
+	    verbose,
+	    write,
+	    useRmax);
   }
 
   const char * parse(const char * arg) {
     switch (arg[0]) {
-    case 'l':
-      return "lattice";
     case 'c':
       return "cube";
-    case 's':
-      return "sphere";
+    case 'l':
+      return "lattice";
     case 'o':
       return "octant";
     case 'p':
       return "plummer";
+    case 's':
+      return "sphere";
     default:
       fprintf(stderr, "invalid distribution %s\n", arg);
       exit(0);
@@ -107,90 +111,90 @@ private:
   }
 
 public:
-  Args(int argc=0, char ** argv=NULL) : numBodies(1000000),
+  Args(int argc=0, char ** argv=NULL) :
+    // In alphabetical order of the short option (group kernel dependent arguments first)
 #if Helmholtz
 					ncrit(1000),
+					dual(0),
+					mutual(0),
+					useRopt(0),
+					theta(1.),
+					useRmax(0),
 #else
 					ncrit(64),
+					dual(1),
+					mutual(1),
+					useRopt(1),
+					theta(.4),
+					useRmax(1),
 #endif
+					distribution("cube"),
+					graft(1),
+					images(0),
+					IneJ(0),
+					numBodies(1000000),
+					repeat(1),
 					nspawn(5000),
 					threads(16),
-					images(0),
-#if Helmholtz
-					theta(1.),
-					dual(0),
-					useRmax(0),
-					useRopt(0),
-					mutual(0),
-#else
-					theta(.4),
-					dual(1),
-					useRmax(1),
-					useRopt(1),
-					mutual(1),
-#endif
-					graft(1),
 					verbose(1),
-					distribution("cube"),
-					repeat(1),
-					IneJ(0),
 					write(0) {
     while (1) {
       int option_index;
-      int c = getopt_long(argc, argv, "n:c:s:T:i:t:D:x:o:m:g:v:d:r:j:w:h", long_options, &option_index);
+      int c = getopt_long(argc, argv, "c:d:Dghi:jmn:or:s:t:T:vwx", long_options, &option_index);
       if (c == -1) break;
       switch (c) {
-      case 'n':
-        numBodies = atoi(optarg);
-        break;
+	// In alphabetical order of the short option
       case 'c':
         ncrit = atoi(optarg);
         break;
-      case 's':
-        nspawn = atoi(optarg);
+      case 'd':
+        distribution = parse(optarg);
         break;
-      case 'T':
-        threads = atoi(optarg);
+      case 'D':
+        dual = 1;
         break;
+      case 'g':
+	graft = 1;
+	break;
+      case 'h':
+        usage(argv[0]);
+        exit(0);
       case 'i':
         images = atoi(optarg);
         break;
-      case 't':
-        theta = atof(optarg);
-        break;
-      case 'D':
-        dual = atof(optarg);
-        break;
-      case 'x':
-        useRmax = atof(optarg);
-        break;
-      case 'o':
-        useRopt = atof(optarg);
+      case 'j':
+        IneJ = 1;
         break;
       case 'm':
-        mutual = atoi(optarg);
+        mutual = 1;
         break;
-      case 'g':
-	graft = atoi(optarg);
-	break;
-      case 'v':
-	verbose= atoi(optarg);
-	break;
-      case 'd':
-        distribution = parse(optarg);
+      case 'n':
+        numBodies = atoi(optarg);
+        break;
+      case 'o':
+        useRopt = 1;
         break;
       case 'r':
         repeat = atoi(optarg);
         break;
-      case 'j':
-        IneJ = atoi(optarg);
+      case 's':
+        nspawn = atoi(optarg);
         break;
+      case 't':
+        theta = atof(optarg);
+        break;
+      case 'T':
+        threads = atoi(optarg);
+        break;
+      case 'v':
+	verbose = 1;
+	break;
       case 'w':
-        write = atoi(optarg);
+        write = 1;
         break;
-      case 'h':
-        usage(argv[0]);
-        exit(0);
+      case 'x':
+        useRmax = atoi(optarg);
+        break;
       default:
         usage(argv[0]);
         exit(0);
@@ -201,39 +205,39 @@ public:
   void print(int stringLength, int P) {
     if (verbose) {                                              // If verbose flag is true
       std::cout << std::setw(stringLength) << std::fixed << std::left// Set format
-		<< "numBodies" << " : " << numBodies << std::endl // Print numBodies  
-		<< std::setw(stringLength)                      //  Set format
-		<< "P" << " : " << P << std::endl               //  Print P
-		<< std::setw(stringLength)                      //  Set format
-		<< "theta" << " : " << theta << std::endl       //  Print theta
-		<< std::setw(stringLength)                      //  Set format
 		<< "ncrit" << " : " << ncrit << std::endl       //  Print ncrit
-		<< std::setw(stringLength)                      //  Set format
-		<< "nspawn" << " : " << nspawn << std::endl     //  Print nspawn
-		<< std::setw(stringLength)                      //  Set format
-		<< "threads" << " : " << threads << std::endl   //  Print threads
-		<< std::setw(stringLength)                      //  Set format
-		<< "images" << " : " << images << std::endl     //  Print images
-		<< std::setw(stringLength)                      //  Set format
-		<< "dual" << " : " << dual << std::endl         //  Print images
-		<< std::setw(stringLength)                      //  Set format
-		<< "useRmax" << " : " << useRmax << std::endl   //  Print useRmax
-		<< std::setw(stringLength)                      //  Set format
-		<< "useRopt" << " : " << useRopt << std::endl   //  Print useRopt
-		<< std::setw(stringLength)                      //  Set format
-		<< "mutual" << " : " << mutual << std::endl     //  Print mutual
-		<< std::setw(stringLength)                      //  Set format
-		<< "graft" << " : " << graft << std::endl       //  Print graft
-		<< std::setw(stringLength)                      //  Set format
-		<< "verbose" << " : " << verbose << std::endl   //  Print verbose
 		<< std::setw(stringLength)                      //  Set format
 		<< "distribution" << " : " << distribution << std::endl// Print distribution
 		<< std::setw(stringLength)                      //  Set format
-		<< "repeat" << " : " << repeat << std::endl     //  Print repeat
+		<< "dual" << " : " << dual << std::endl         //  Print images
+		<< std::setw(stringLength)                      //  Set format
+		<< "graft" << " : " << graft << std::endl       //  Print graft
+		<< std::setw(stringLength)                      //  Set format
+		<< "images" << " : " << images << std::endl     //  Print images
 		<< std::setw(stringLength)                      //  Set format
 		<< "IneJ" << " : " << IneJ << std::endl         //  Print IneJ
 		<< std::setw(stringLength)                      //  Set format
-		<< "write" << " : " << write << std::endl;      //  Print write
+		<< "mutual" << " : " << mutual << std::endl     //  Print mutual
+		<< std::setw(stringLength)                      //  Set format
+		<< "numBodies" << " : " << numBodies << std::endl// Print numBodies  
+		<< std::setw(stringLength)                      //  Set format
+		<< "useRopt" << " : " << useRopt << std::endl   //  Print useRopt
+		<< std::setw(stringLength)                      //  Set format
+		<< "P" << " : " << P << std::endl               //  Print P
+		<< std::setw(stringLength)                      //  Set format
+		<< "repeat" << " : " << repeat << std::endl     //  Print repeat
+		<< std::setw(stringLength)                      //  Set format
+		<< "nspawn" << " : " << nspawn << std::endl     //  Print nspawn
+		<< std::setw(stringLength)                      //  Set format
+		<< "theta" << " : " << theta << std::endl       //  Print theta
+		<< std::setw(stringLength)                      //  Set format
+		<< "threads" << " : " << threads << std::endl   //  Print threads
+		<< std::setw(stringLength)                      //  Set format
+		<< "verbose" << " : " << verbose << std::endl   //  Print verbose
+		<< std::setw(stringLength)                      //  Set format
+		<< "write" << " : " << write << std::endl       //  Print write
+		<< std::setw(stringLength)                      //  Set format
+		<< "useRmax" << " : " << useRmax << std::endl;  //  Print useRmax
     }                                                           // End if for verbose flag
   }
 };
