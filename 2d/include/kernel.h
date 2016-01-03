d0f024003beb522483e3cedd226c1ef3eda02bf0
Jeff Harris <clark115@gmail.com>
2016-01-03 02:32:33 +0100

    Debug 2D kernel, change from Beatson/Greengard to Liu/Nishimura expansions

--- 2d/include/kernel.h
+++ 2d/include/kernel.h
@@ -286,108 +286,96 @@ class Kernel {
 
 //!< P2M kernel for cell C
   void P2M(C_iter C) const {
-    for (B_iter B=C->BODY; B!=C->BODY+C->NCBODY; B++) {
-      vec2 dX = B->X - C->X;
-      complex_t Z(dX[0],dX[1]), powZ(1.0, 0.0);
-      vecP M;
-      C->M[0] += B->SRC;
-      for (int n=1; n<P; n++) {
-        powZ *= Z;
-        C->M[n] += -powZ * real_t(B->SRC / n);
-      }
-    }
+    for (B_iter B=C->BODY; B!=C->BODY+C->NCBODY; B++) {         // Loop over bodies
+      vec2 dX = B->X - C->X;                                    //  Get distance vector
+      complex_t Z(dX[0],dX[1]), powZ(1.0, 0.0);                 //  Convert to complex plane
+      C->M[0] += B->SRC;                                        //  Add constant term
+      for (int n=1; n<P; n++) {                                 //  Loop over coefficients
+        powZ *= Z / real_t(n);                                  //   Store z^n / n!
+        C->M[n] += powZ * B->SRC;                               //   Add to coefficient
+      }                                                         //  End loop
+    }                                                           // End loop
   }
 
 //!< M2M kernel for one parent cell Ci
   void M2M(C_iter Ci, C_iter C0) const {
-    for (C_iter Cj=C0+Ci->CHILD; Cj!=C0+Ci->CHILD+Ci->NCHILD; Cj++) {
-      vec2 dX = Cj->X - Ci->X;
-      complex_t Z(dX[0],dX[1]), powZn(1.0, 0.0), powZnk(1.0, 0.0), invZ(powZn/Z);
-      Ci->M[0] += Cj->M[0];
-      for (int n=1; n<P; n++) {
-        powZn *= Z;
-	Ci->M[n] -= Cj->M[0] * powZn / real_t(n);
-        powZnk = powZn;
-        real_t Cnk = 1.0;
-	for (int k=1; k<=n; k++) {
-          powZnk *= invZ;
-	  Ci->M[n] += Cj->M[k] * powZnk * Cnk;
-          Cnk *= real_t(n - k) / k;
-	}
-      }
-    }
+    for (C_iter Cj=C0+Ci->CHILD;
+	 Cj!=C0+Ci->CHILD+Ci->NCHILD; Cj++) {                   // Loop over child cells
+      vec2 dX = Cj->X - Ci->X;                                  //  Get distance vector
+      complex_t Z(dX[0],dX[1]), powZn(1.0, 0.0),
+	powZnk(1.0, 0.0), invZ(powZn/Z);                        //  Convert to complex plane
+      for (int k=0; k<P; k++) {                                 //  Loop over coefficients
+	complex_t powZ(1.0, 0.0);                               //   z^0 = 1
+	Ci->M[k] += Cj->M[k];                                   //   Add constant term
+	for (int kml=1; kml<=k; kml++) {                        //   Loop over k-l
+	  powZ *= Z / real_t(kml);                              //    Store z^(k-l) / (k-l)!
+	  Ci->M[k] += Cj->M[k-kml] * powZ;                      //    Add to coefficient
+	}                                                       //   End loop
+      }                                                         //  End loop
+    }                                                           // End loop
   }
 
 //!< M2L kernel between cells Ci and Cj
   void M2L(C_iter Ci, C_iter Cj, bool mutual) const {
-    vec2 dX = Ci->X - Cj->X - Xperiodic;
-    complex_t Z(dX[0],dX[1]), powZn(1.0, 0.0), powZnk(1.0, 0.0), invZ(powZn/Z);
-    Ci->L[0] += Cj->M[0] * log(Z);
-    for (int k=1; k<P; k++) {
-      powZn *= invZ;
-      Ci->L[0] += Cj->M[k] * powZn;
-    }
-    powZn = complex_t(1.0, 0.0);
-    for (int n=1; n<P; n++) {
-      powZn *= -invZ;
-      Ci->L[n] -= Cj->M[0] * powZn / real_t(n);
-      powZnk = powZn;
-      real_t Cnk = 1.0;
-      for (int k=1; k<P; k++) {
-        powZnk *= invZ;
-	Ci->L[n] += Cj->M[k] * powZnk * Cnk;
-        Cnk *= real_t(n + 1) / k;
-      }
-    }
-    if (mutual) {
-      Cj->L[0] += Ci->M[0] * log(-Z);
-      powZn = complex_t(1.0, 0.0);
-      for (int k=1; k<P; k++) {
-        powZn *= -invZ;
-	Cj->L[0] += Ci->M[k] * powZn;
-      }
-      powZn = complex_t(1.0, 0.0);
-      for (int n=1; n<P; n++) {
-        powZn *= invZ;
-	Cj->L[n] -= Ci->M[0] * powZn / real_t(n);
-        powZnk = powZn;
-        real_t Cnk = 1.0;
-	for (int k=1; k<P; k++) {
-          powZnk *= -invZ;
-	  Cj->L[n] += Ci->M[k] * powZnk * Cnk;
-          Cnk *= real_t(n + 1) / k;
-	}
-      }
-    }
+    vec2 dX = Ci->X - Cj->X - Xperiodic;                        // Get distance vector
+    complex_t Z(dX[0],dX[1]), powZn(1.0, 0.0),
+      powZnk(1.0, 0.0), invZ(powZn/Z);                          // Convert to complex plane
+    Ci->L[0] += -Cj->M[0] * log(Z);                             // Log term (for 0th order)
+    Ci->L[0] += Cj->M[1] * invZ;                                // Constant term
+    powZn = invZ;                                               // 1/z term
+    for (int k=2; k<P; k++) {                                   // Loop over coefficients
+      powZn *= real_t(k-1)*invZ;                                //  Store (k-1)! / z^k
+      Ci->L[0] += Cj->M[k] * powZn;                             //  Add to coefficient
+    }                                                           // End loop
+    Ci->L[1] += -Cj->M[0] * invZ;                               // Constant term (for 1st order)
+    powZn = invZ;                                               // 1/z term
+    for (int k=1; k<P; k++) {                                   // Loop over coefficients
+      powZn *= real_t(1+k-1)*invZ;                              //  Store (k)! / z^k
+      Ci->L[1] += -Cj->M[k] * powZn;                            //  Add to coefficient
+    }                                                           // End loop
+    real_t Cnk = -1;                                            // Fix sign term
+    for (int n=2; n<P; n++) {                                   // Loop over 
+      Cnk *= -1;                                                //  Flip sign
+      powZnk *= invZ;                                           //  Store 1 / z^n
+      powZn = Cnk*powZnk;                                       //  Combine terms
+      for (int k=0; k<P; k++) {                                 //  Loop over
+	powZn *= real_t(n+k-1)*invZ;                            //   (n+k-1)! / z^k
+	Ci->L[n] += Cj->M[k] * powZn;                           //   Add to coefficient
+      }                                                         //  End loop
+      powZnk *= real_t(n-1);                                    //  Store (n-1)! / z^n
+    }                                                           // End loop
+    if (mutual) {                                               // If mutual interaction
+      M2L(Cj,Ci,false);                                         //  Lazy recursion
+    }                                                           // End if
   }
 
 //!< L2L kernel for one child cell Ci
   void L2L(C_iter Ci, C_iter C0) const {
-    C_iter Cj = C0 + Ci->PARENT;
-    vec2 dX = Ci->X - Cj->X;
-    complex_t Z(dX[0],dX[1]);
-    for (int n=0; n<P; n++) {
-      complex_t powZ(1.0, 0.0);
-      real_t Cnk = 1.0;
-      for (int k=n; k<P; k++) {
-	Ci->L[n] += Cj->L[k] * powZ * Cnk;
-        powZ *= Z;
-        Cnk *= real_t(k + 1) / (k - n + 1);
-      }
-    }
+    C_iter Cj = C0 + Ci->PARENT;                                // Get parent cell
+    vec2 dX = Ci->X - Cj->X;                                    // Get distance vector
+    complex_t Z(dX[0],dX[1]);                                   // Convert to complex plane
+    for (int l=0; l<P; l++) {                                   // Loop over coefficients
+      complex_t powZ(1.0, 0.0);                                 //  z^0 = 1
+      Ci->L[l] += Cj->L[l];                                     //  Add constant term
+      for (int k=1; k<P-l; k++) {                               //  Loop over coefficients
+	powZ *= Z / real_t(k);                                  //   Store z^k / k!
+	Ci->L[l] += Cj->L[l+k] * powZ;                          //   Add to coefficient
+      }                                                         //  End loop
+    }                                                           // End loop
   }
 
 //!< L2P kernel for cell Ci
   void L2P(C_iter Ci) const {
-    for (B_iter B=Ci->BODY; B!=Ci->BODY+Ci->NCBODY; B++) {
-      vec2 dX = B->X - Ci->X;
-      complex_t Z(dX[0],dX[1]), powZ(1.0, 0.0);
-      B->TRG /= B->SRC;
-      for (int n=0; n<P; n++) {
-        B->TRG -= std::real(Ci->L[n] * powZ);
-        powZ *= Z;
-      }
-    }
+    for (B_iter B=Ci->BODY; B!=Ci->BODY+Ci->NCBODY; B++) {      // Loop over bodies
+      vec2 dX = B->X - Ci->X;                                   //  Get distance vector
+      complex_t Z(dX[0],dX[1]), powZ(1.0, 0.0);                 //  Convert to complex plane
+      B->TRG /= B->SRC;                                         //  Normalize result
+      B->TRG += std::real(Ci->L[0]);                            //  Add constant term
+      for (int n=1; n<P; n++) {                                 //  Loop over coefficients
+        powZ *= Z / real_t(n);                                  //   Store z^n / n!
+        B->TRG += std::real(Ci->L[n] * powZ);                   //   Add real part to solution
+      }                                                         //  End loop
+    }                                                           // End loop
   }
 };
 #endif
