0015e8f6689eab4bc6ec1b94c3b52ddb502f3274
Hideo Doi <dddoooiii888@gmail.com>
2016-03-21 23:59:36 +0900

    add md_sample

---
+++ wrappers/run_md.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+make
+make -f Makefile.testmd
+
+rm 0*
+mpirun.mpich -np 1 ./sample.exe test.inp
+diff 01_energy.log 05_mpi_ene_00.dat > log.diff1
+mpirun.mpich -np 2 ./sample.exe test.inp
+diff 01_energy.log 05_mpi_ene_00.dat > log.diff2
+
