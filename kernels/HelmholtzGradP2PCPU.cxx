09feb4aa5cd40a8a5858fdf597bc0f1ae578a262
Rio Yokota <rioyokota@gmail.com>
2016-03-27 20:39:19 +0900

    Tab to spaces

--- kernels/HelmholtzGradP2PCPU.cxx
+++ kernels/HelmholtzGradP2PCPU.cxx
@@ -17,31 +17,31 @@ namespace exafmm {
       int nj = Cj->NBODY;
       int i = 0;
       for ( ; i<ni; i++) {
-	complex_t pot(0.0, 0.0);
-	complex_t ax(0.0, 0.0);
-	complex_t ay(0.0, 0.0);
-	complex_t az(0.0, 0.0);
-	complex_t mi = Bi[i].SRC;
-	for (int j=0; j<nj; j++) {
-	  complex_t mj = Bj[j].SRC;
-	  vec3 dX = Bi[i].X - Bj[j].X - Xperiodic;
-	  real_t R2 = norm(dX) + eps2;
-	  if (R2 != 0) {
-	    real_t R = sqrt(R2);
-	    complex_t src2 = mi * mj;
-	    complex_t expikr = std::exp(I * wavek * R) / R;
-	    complex_t coef1 = src2 * expikr;
-	    complex_t coef2 = (one - I * wavek * R) / R2 * coef1;
-	    pot += coef1;
-	    ax += coef2 * dX[0];
-	    ay += coef2 * dX[1];
-	    az += coef2 * dX[2];
-	  }
-	}
-	Bi[i].TRG[0] += pot;
-	Bi[i].TRG[1] += ax;
-	Bi[i].TRG[2] += ay;
-	Bi[i].TRG[3] += az;
+        complex_t pot(0.0, 0.0);
+        complex_t ax(0.0, 0.0);
+        complex_t ay(0.0, 0.0);
+        complex_t az(0.0, 0.0);
+        complex_t mi = Bi[i].SRC;
+        for (int j=0; j<nj; j++) {
+          complex_t mj = Bj[j].SRC;
+          vec3 dX = Bi[i].X - Bj[j].X - Xperiodic;
+          real_t R2 = norm(dX) + eps2;
+          if (R2 != 0) {
+            real_t R = sqrt(R2);
+            complex_t src2 = mi * mj;
+            complex_t expikr = std::exp(I * wavek * R) / R;
+            complex_t coef1 = src2 * expikr;
+            complex_t coef2 = (one - I * wavek * R) / R2 * coef1;
+            pot += coef1;
+            ax += coef2 * dX[0];
+            ay += coef2 * dX[1];
+            az += coef2 * dX[2];
+          }
+        }
+        Bi[i].TRG[0] += pot;
+        Bi[i].TRG[1] += ax;
+        Bi[i].TRG[2] += ay;
+        Bi[i].TRG[3] += az;
       }
     }
 
@@ -56,11 +56,11 @@ namespace exafmm {
         complex_t ay(0.0, 0.0);
         complex_t az(0.0, 0.0);
         complex_t mi = B[i].SRC;
-	      for (int j=i+1; j<n; j++) {
+        for (int j=i+1; j<n; j++) {
           complex_t mj = B[j].SRC;
-	        vec3 dX = B[j].X - B[i].X;
-	        real_t R2 = norm(dX) + eps2;
-	        real_t R = sqrt(R2);
+          vec3 dX = B[j].X - B[i].X;
+          real_t R2 = norm(dX) + eps2;
+          real_t R = sqrt(R2);
           complex_t src2 = mi * mj;
           complex_t expikr = std::exp(I * wavek * R) / R;
           complex_t coef1 = src2 * expikr;
@@ -73,7 +73,7 @@ namespace exafmm {
           B[i].TRG[1] += coef2 * dX[0];
           B[i].TRG[2] += coef2 * dX[1];
           B[i].TRG[3] += coef2 * dX[2];
-	      }
+        }
         B[i].TRG[0] += pot;
         B[i].TRG[1] += ax;
         B[i].TRG[2] += ay;
