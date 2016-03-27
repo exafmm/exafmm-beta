d5542b499ef528045798207d68d83d1f95aa9d1e
Rio Yokota <rioyokota@gmail.com>
2016-03-27 19:41:23 +0900

    Simplified HelmholtzGrad

--- kernels/HelmholtzGradP2PCPU.cxx
+++ kernels/HelmholtzGradP2PCPU.cxx
@@ -17,7 +17,8 @@ namespace exafmm {
       int ni = Ci->NBODY;
       int nj = Cj->NBODY;
       int i = 0;
-#if EXAFMM_USE_SIMD
+      //#if EXAFMM_USE_SIMD
+#if 0
       simdvec wave_rvec = wave_r;
       simdvec wave_ivec = wave_i;
       for ( ; i<=ni-NSIMD; i+=NSIMD) {
@@ -100,54 +101,31 @@ namespace exafmm {
       }
 #endif
       for ( ; i<ni; i++) {
-	real_t pot_r = 0.0;
-	real_t pot_i = 0.0;
-	real_t ax_r = 0.0;
-	real_t ax_i = 0.0;
-	real_t ay_r = 0.0;
-	real_t ay_i = 0.0;
-	real_t az_r = 0.0;
-	real_t az_i = 0.0;
-	real_t mi_r = std::real(Bi[i].SRC);
-	real_t mi_i = std::imag(Bi[i].SRC);
+	complex_t pot(0.0, 0.0);
+	complex_t ax(0.0, 0.0);
+	complex_t ay(0.0, 0.0);
+	complex_t az(0.0, 0.0);
+	complex_t mi = Bi[i].SRC;
 	for (int j=0; j<nj; j++) {
-	  real_t mj_r = std::real(Bj[j].SRC);
-	  real_t mj_i = std::imag(Bj[j].SRC);
+	  complex_t mj = Bj[j].SRC;
 	  vec3 dX = Bi[i].X - Bj[j].X - Xperiodic;
 	  real_t R2 = norm(dX) + eps2;
 	  if (R2 != 0) {
 	    real_t R = sqrt(R2);
-	    real_t src2_r = mi_r * mj_r - mi_i * mj_i;
-	    real_t src2_i = mi_r * mj_i + mi_i * mj_r;
-	    real_t expikr = std::exp(wave_i * R) * R;
-	    real_t expikr_r = std::cos(wave_r * R) / expikr;
-	    real_t expikr_i = std::sin(wave_r * R) / expikr;
-	    real_t coef1_r = src2_r * expikr_r - src2_i * expikr_i;
-	    real_t coef1_i = src2_r * expikr_i + src2_i * expikr_r;
-	    real_t kr_r = (1 + wave_i * R) / R2;
-	    real_t kr_i = - wave_r / R;
-	    real_t coef2_r = kr_r * coef1_r - kr_i * coef1_i;
-	    real_t coef2_i = kr_r * coef1_i + kr_i * coef1_r;
-	    pot_r += coef1_r;
-	    pot_i += coef1_i;
-	    ax_r += coef2_r * dX[0];
-	    ax_i += coef2_i * dX[0];
-	    ay_r += coef2_r * dX[1];
-	    ay_i += coef2_i * dX[1];
-	    az_r += coef2_r * dX[2];
-	    az_i += coef2_i * dX[2];
-	    if (mutual) {
-	      Bj[j].TRG[0] += complex_t(coef1_r, coef1_i);
-	      Bj[j].TRG[1] += complex_t(coef2_r, coef2_i) * dX[0];
-	      Bj[j].TRG[2] += complex_t(coef2_r, coef2_i) * dX[1];
-	      Bj[j].TRG[3] += complex_t(coef2_r, coef2_i) * dX[2];
-	    }
+	    complex_t src2 = mi * mj;
+	    complex_t expikr = std::exp(I * wavek * R) / R;
+	    complex_t coef1 = src2 * expikr;
+	    complex_t coef2 = (1 - I * wavek * R) / R2 * coef1;
+	    pot += coef1;
+	    ax += coef2 * dX[0];
+	    ay += coef2 * dX[1];
+	    az += coef2 * dX[2];
 	  }
 	}
-	Bi[i].TRG[0] += complex_t(pot_r, pot_i);
-	Bi[i].TRG[1] += complex_t(ax_r, ax_i);
-	Bi[i].TRG[2] += complex_t(ay_r, ay_i);
-	Bi[i].TRG[3] += complex_t(az_r, az_i);
+	Bi[i].TRG[0] += pot;
+	Bi[i].TRG[1] += ax;
+	Bi[i].TRG[2] += ay;
+	Bi[i].TRG[3] += az;
       }
     }
 
@@ -157,7 +135,8 @@ namespace exafmm {
       B_iter B = C->BODY;
       int n = C->NBODY;
       int i = 0;
-#if EXAFMM_USE_SIMD
+      //#if EXAFMM_USE_SIMD
+#if 0
       simdvec wave_rvec = wave_r;
       simdvec wave_ivec = wave_i;
       for ( ; i<=n-NSIMD; i+=NSIMD) {
