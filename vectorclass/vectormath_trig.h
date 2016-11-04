e642a5ca54e900dfa642a2d8170d534c64eb9b05
Rio Yokota <rioyokota@gmail.com>
2016-11-04 23:49:24 +0900

    Gradually removing vectorclass again: Step 17

--- vectorclass/vectormath_trig.h
+++ vectorclass/vectormath_trig.h
@@ -20,6 +20,9 @@ static inline __m128i vec_neq(__m128i const & a, int const & b) {
 static inline __m128i vec_lt(__m128i const & a, int const & b) {
   return _mm_cmpgt_epi32(_mm_set1_epi32(b), a);
 }
+static inline Vec2db vec_lt(__m128d const & a, double const & b) {
+  return _mm_cmplt_pd(_mm_set1_pd(b), a);
+}
 static inline __m128i vec_sll(__m128i const & a, int const & b) {
   return _mm_sll_epi32(a,_mm_cvtsi32_si128(b));
 }
@@ -38,6 +41,7 @@ static inline __m128i is_finite(__m128 const & a) {
 static inline bool horizontal_or (__m128i const & a) {
   return ! _mm_testz_si128(a,a);
 }
+
 static inline Vec4i vm_truncate_low_to_int(Vec2d const & x) {
   return truncate_to_int(x,x);
 }
@@ -55,7 +59,6 @@ template<>
 inline Vec2q vm_half_int_vector_to_full<Vec2q,Vec4i>(Vec4i const & x) {
   return extend_low(x);
 }
-
 #if __AVX__
 static inline __m256i vec_and(__m256i const & a, int const & b) {
   return _mm256_and_si256(a,_mm256_set1_epi32(b));
@@ -75,6 +78,9 @@ static inline __m256i vec_neq(__m256i const & a, int const & b) {
 static inline __m256i vec_lt(__m256i const & a, int const & b) {
   return _mm256_cmpgt_epi32(_mm256_set1_epi32(b), a);
 }
+static inline Vec4db vec_lt(__m256d const & a, double const & b) {
+  return _mm256_cmp_pd(_mm256_set1_pd(b), a, 1);
+}
 static inline __m256i vec_sll(__m256i const & a, int const & b) {
   return _mm256_sll_epi32(a, _mm_cvtsi32_si128(b));
 }
@@ -91,6 +97,7 @@ static inline __m256i is_finite(__m256 const & a) {
 static inline bool horizontal_or (__m256i const & a) {
   return ! _mm256_testz_si256(a,a);
 }
+
 static inline Vec4i vm_truncate_low_to_int(Vec4d const & x) {
   return truncate_to_int(x);
 }
@@ -121,7 +128,10 @@ static inline __mmask16 vec_neq(__m512i const & a, int const & b) {
   return _mm512_cmpneq_epi32_mask(a,_mm512_set1_epi32(b));
 }
 static inline __mmask16 vec_lt(__m512i const & a, int const & b) {
-  return Vec16fb(_mm512_cmpgt_epi32_mask(_mm512_set1_epi32(b), a));
+  return _mm512_cmpgt_epi32_mask(_mm512_set1_epi32(b), a);
+}
+static inline Vec8db vec_lt(__m512d const & a, double const & b) {
+  return _mm512_cmp_pd_mask(a, _mm512_set1_pd(b), 1);
 }
 static inline __m512i vec_sll(__m512i const & a, int const & b) {
   return _mm512_sll_epi32(a, _mm_cvtsi32_si128(b));
@@ -139,6 +149,7 @@ static inline __mmask16 is_finite(__m512 const & a) {
 static inline bool horizontal_or (__mmask16 const & a) {
   return (uint16_t)a != 0;
 }
+
 static inline Vec8i vm_truncate_low_to_int(Vec8d const & x) {
   return truncate_to_int(x);
 }
@@ -207,14 +218,14 @@ static inline VTYPE sincos_f(VTYPE * cosret, VTYPE const & xx) {
     }
 
     if (SC & 5) {  // calculate sin
-        sin1 = select(swap, c, s);
-        signsin = vec_and(vec_xor(vec_sll(q,29),reinterpret_i(xx)),1 << 31);
-        sin1 = vec_xor(sin1,reinterpret_f(signsin));
+      sin1 = select(swap, c, s);
+      signsin = vec_and(vec_xor(vec_sll(q,29),reinterpret_i(xx)),1 << 31);
+      sin1 = vec_xor(sin1,reinterpret_f(signsin));
     }
     if (SC & 6) {  // calculate cos
-        cos1 = select(swap, s, c);
-        signcos = vec_and(vec_sll(q+2,29),1 << 31);
-        cos1 = vec_xor(cos1,reinterpret_f(signcos));
+      cos1 = select(swap, s, c);
+      signcos = vec_and(vec_sll(q+2,29),1 << 31);
+      cos1 = vec_xor(cos1,reinterpret_f(signcos));
     }
     if      (SC == 1) return sin1;
     else if (SC == 2) return cos1;
@@ -285,99 +296,100 @@ static inline float _mm512_reduce_add_ps(__m512 const & in) {
 }
 #endif
 
-template<class VTYPE, class ITYPE, class ITYPEH, class BVTYPE, int SC>
-static inline VTYPE sincos_d(VTYPE * cosret, VTYPE const & xx) {
+template<class VTYPE2, class VTYPE, class ITYPE, class ITYPEH, class BVTYPE, int SC>
+static inline VTYPE sincos_d(VTYPE2 * cosret, VTYPE const & xx) {
     // define constants
-    const double ONEOPIO4 = 4./VM_PI;
+  const double ONEOPIO4 = 4./VM_PI;
 
-    const double P0sin =-1.66666666666666307295E-1;
-    const double P1sin = 8.33333333332211858878E-3;
-    const double P2sin =-1.98412698295895385996E-4;
-    const double P3sin = 2.75573136213857245213E-6;
-    const double P4sin =-2.50507477628578072866E-8;
-    const double P5sin = 1.58962301576546568060E-10;
+  const double P0sin =-1.66666666666666307295E-1;
+  const double P1sin = 8.33333333332211858878E-3;
+  const double P2sin =-1.98412698295895385996E-4;
+  const double P3sin = 2.75573136213857245213E-6;
+  const double P4sin =-2.50507477628578072866E-8;
+  const double P5sin = 1.58962301576546568060E-10;
 
-    const double P0cos = 4.16666666666665929218E-2;
-    const double P1cos =-1.38888888888730564116E-3;
-    const double P2cos = 2.48015872888517045348E-5;
-    const double P3cos =-2.75573141792967388112E-7;
-    const double P4cos = 2.08757008419747316778E-9;
-    const double P5cos =-1.13585365213876817300E-11;
+  const double P0cos = 4.16666666666665929218E-2;
+  const double P1cos =-1.38888888888730564116E-3;
+  const double P2cos = 2.48015872888517045348E-5;
+  const double P3cos =-2.75573141792967388112E-7;
+  const double P4cos = 2.08757008419747316778E-9;
+  const double P5cos =-1.13585365213876817300E-11;
 
-    const double DP1 = 7.853981554508209228515625E-1;
-    const double DP2 = 7.94662735614792836714E-9;
-    const double DP3 = 3.06161699786838294307E-17;
-    /*
+  const double DP1 = 7.853981554508209228515625E-1;
+  const double DP2 = 7.94662735614792836714E-9;
+  const double DP3 = 3.06161699786838294307E-17;
+  /*
     const double DP1sc = 7.85398125648498535156E-1;
     const double DP2sc = 3.77489470793079817668E-8;
     const double DP3sc = 2.69515142907905952645E-15;
-    */
-    VTYPE  xa, x, y, x2, s, c, sin1, cos1;       // data vectors
-    ITYPEH q;                                    // integer vectors, 32 bit
-    ITYPE  qq, signsin, signcos;                 // integer vectors, 64 bit
-    BVTYPE swap, overflow;                       // boolean vectors
+  */
+  VTYPE  x2, s, c, sin1, cos1;       // data vectors
+  VTYPE2 xa, x, y;
+  ITYPEH q;                                    // integer vectors, 32 bit
+  ITYPE  qq, signsin, signcos;                 // integer vectors, 64 bit
+  BVTYPE swap, overflow;                       // boolean vectors
 
-    xa = abs(xx);
+  xa = abs(xx);
 
-    // Find quadrant
-    //      0 -   pi/4 => 0
-    //   pi/4 - 3*pi/4 => 2
-    // 3*pi/4 - 5*pi/4 => 4
-    // 5*pi/4 - 7*pi/4 => 6
-    // 7*pi/4 - 8*pi/4 => 8
+  // Find quadrant
+  //      0 -   pi/4 => 0
+  //   pi/4 - 3*pi/4 => 2
+  // 3*pi/4 - 5*pi/4 => 4
+  // 5*pi/4 - 7*pi/4 => 6
+  // 7*pi/4 - 8*pi/4 => 8
 
-    // truncate to integer (magic number conversion is not faster here)
-    q = vm_truncate_low_to_int(xa * ONEOPIO4);
-    q = (q + 1) & ~1;
+  // truncate to integer (magic number conversion is not faster here)
+  q = vm_truncate_low_to_int(xa * ONEOPIO4);
+  q = (q + 1) & ~1;
 
-    y = vm_half_int_vector_to_double<VTYPE>(q);  // quadrant, as double
+  y = vm_half_int_vector_to_double<VTYPE>(q);  // quadrant, as double
 
-    // Reduce by extended precision modular arithmetic
-    x = nmul_add(y, DP3, nmul_add(y, DP2, nmul_add(y, DP1, xa)));    // x = ((xa - y * DP1) - y * DP2) - y * DP3;
+  // Reduce by extended precision modular arithmetic
+  x = nmul_add(y, DP3, nmul_add(y, DP2, nmul_add(y, DP1, xa)));    // x = ((xa - y * DP1) - y * DP2) - y * DP3;
 
-    // Expansion of sin and cos, valid for -pi/4 <= x <= pi/4
-    x2 = x * x;
-    s = polynomial_5(x2, P0sin, P1sin, P2sin, P3sin, P4sin, P5sin);
-    c = polynomial_5(x2, P0cos, P1cos, P2cos, P3cos, P4cos, P5cos);
-    s = mul_add(x * x2, s, x);                                       // s = x + (x * x2) * s;
-    c = mul_add(x2 * x2, c, nmul_add(x2, 0.5, 1.0));                 // c = 1.0 - x2 * 0.5 + (x2 * x2) * c;
+  // Expansion of sin and cos, valid for -pi/4 <= x <= pi/4
+  x2 = x * x;
+  s = polynomial_5(x2, P0sin, P1sin, P2sin, P3sin, P4sin, P5sin);
+  c = polynomial_5(x2, P0cos, P1cos, P2cos, P3cos, P4cos, P5cos);
+  s = mul_add(x * x2, s, x);                                       // s = x + (x * x2) * s;
+  c = mul_add(x2 * x2, c, nmul_add(x2, 0.5, 1.0));                 // c = 1.0 - x2 * 0.5 + (x2 * x2) * c;
 
-    // correct for quadrant
-    qq = vm_half_int_vector_to_full<ITYPE,ITYPEH>(q);
-    swap = BVTYPE((qq & 2) != 0);
+  // correct for quadrant
+  qq = vm_half_int_vector_to_full<ITYPE,ITYPEH>(q);
+  swap = BVTYPE((qq & 2) != 0);
 
-    // check for overflow
-    if (horizontal_or(q < 0)) {
-        overflow = (y < 0) & is_finite(xa);
-        s = select(overflow, 0., s);
-        c = select(overflow, 1., c);
-    }
-    if (SC & 1) {  // calculate sin
-        sin1 = select(swap, c, s);
-        signsin = ((qq << 61) ^ ITYPE(reinterpret_i(xx))) & ITYPE(1ULL << 63);
-        sin1 ^= reinterpret_d(signsin);
-    }
-    if (SC & 2) {  // calculate cos
-        cos1 = select(swap, s, c);
-        signcos = ((qq + 2) << 61) & (1ULL << 63);
-        cos1 ^= reinterpret_d(signcos);
-    }
-    if (SC == 3) {  // calculate both. cos returned through pointer
-        *cosret = cos1;
-    }
-    if (SC & 1) return sin1; else return cos1;
+  // check for overflow
+  if (horizontal_or(q < 0)) {
+    overflow = vec_lt(y,0) & is_finite(xa);
+    s = select(overflow, 0., s);
+    c = select(overflow, 1., c);
+  }
+  if (SC & 1) {  // calculate sin
+    sin1 = select(swap, c, s);
+    signsin = ((qq << 61) ^ ITYPE(reinterpret_i(xx))) & ITYPE(1ULL << 63);
+    sin1 ^= reinterpret_d(signsin);
+  }
+  if (SC & 2) {  // calculate cos
+    cos1 = select(swap, s, c);
+    signcos = ((qq + 2) << 61) & (1ULL << 63);
+    cos1 ^= reinterpret_d(signcos);
+  }
+  if (SC == 3) {  // calculate both. cos returned through pointer
+    *cosret = cos1;
+  }
+  if (SC & 1) return sin1; else return cos1;
 }
 
 // instantiations of sincos_d template:
 
 static inline __m128d _mm_sin_pd(__m128d const & x) {
-  return sincos_d<Vec2d, Vec2q, Vec4i, Vec2db, 1>(0, x);
+  return sincos_d<__m128d, Vec2d, Vec2q, Vec4i, Vec2db, 1>(0, x);
 }
 static inline __m128d _mm_cos_pd(__m128d const & x) {
-  return sincos_d<Vec2d, Vec2q, Vec4i, Vec2db, 2>(0, x);
+  return sincos_d<__m128d, Vec2d, Vec2q, Vec4i, Vec2db, 2>(0, x);
 }
 static inline __m128d _mm_sincos_pd(__m128d * cosret, __m128d const & x) {
-  return sincos_d<Vec2d, Vec2q, Vec4i, Vec2db, 3>((Vec2d*)cosret, x);
+  return sincos_d<__m128d, Vec2d, Vec2q, Vec4i, Vec2db, 3>(cosret, x);
 }
 static inline double _mm_reduce_add_pd(__m128d const & in) {
   union {
@@ -390,13 +402,13 @@ static inline double _mm_reduce_add_pd(__m128d const & in) {
 
 #ifdef __AVX__
 static inline __m256d _mm256_sin_pd(__m256d const & x) {
-  return sincos_d<Vec4d, Vec4q, Vec4i, Vec4db, 1>(0, x);
+  return sincos_d<__m256d, Vec4d, Vec4q, Vec4i, Vec4db, 1>(0, x);
 }
 static inline __m256d _mm256_cos_pd(__m256d const & x) {
-  return sincos_d<Vec4d, Vec4q, Vec4i, Vec4db, 2>(0, x);
+  return sincos_d<__m256d, Vec4d, Vec4q, Vec4i, Vec4db, 2>(0, x);
 }
 static inline __m256d _mm256_sincos_pd(__m256d * cosret, __m256d const & x) {
-  return sincos_d<Vec4d, Vec4q, Vec4i, Vec4db, 3>((Vec4d*)cosret, x);
+  return sincos_d<__m256d, Vec4d, Vec4q, Vec4i, Vec4db, 3>(cosret, x);
 }
 static inline double _mm256_reduce_add_pd(__m256d const & in) {
   union {
@@ -412,13 +424,13 @@ static inline double _mm256_reduce_add_pd(__m256d const & in) {
 
 #if __AVX512F__ | __MIC__
 static inline __m512d _mm512_sin_pd(__m512d const & x) {
-  return sincos_d<Vec8d, Vec8q, Vec8i, Vec8db, 1>(0, x);
+  return sincos_d<__m512d, Vec8d, Vec8q, Vec8i, Vec8db, 1>(0, x);
 }
 static inline __m512d _mm512_cos_pd(__m512d const & x) {
-  return sincos_d<Vec8d, Vec8q, Vec8i, Vec8db, 2>(0, x);
+  return sincos_d<__m512d, Vec8d, Vec8q, Vec8i, Vec8db, 2>(0, x);
 }
 static inline __m512d _mm512_sincos_pd(__m512d * cosret, __m512d const & x) {
-  return sincos_d<Vec8d, Vec8q, Vec8i, Vec8db, 3>((Vec8d*)cosret, x);
+  return sincos_d<__m512d, Vec8d, Vec8q, Vec8i, Vec8db, 3>(cosret, x);
 }
 static inline double _mm512_reduce_add_pd(__m512d const & in) {
   __m256d temp = _mm512_extractf64x4_pd(in,1);
