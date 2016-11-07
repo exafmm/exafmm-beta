99c57158d809c23e5eed35705ef5b99ab0bc5676
Rio Yokota <rioyokota@gmail.com>
2016-11-07 21:51:01 +0900

    Gradually removing vectorclass again: Step 45

--- vectorclass/vectormath_exp.h
+++ vectorclass/vectormath_exp.h
@@ -2,6 +2,7 @@
 #define VECTORMATH_EXP_H  1
 
 #include "vectormath_common.h"
+//#include "vectormath_trig.h"
 
 static inline Vec2d vec_pow2n (Vec2d const & n) {
   const double pow2_52 = 4503599627370496.0;
@@ -12,7 +13,6 @@ static inline Vec2d vec_pow2n (Vec2d const & n) {
   Vec2d d = reinterpret_d(c);
   return d;
 }
-
 static inline Vec4f vec_pow2n (Vec4f const & n) {
   const float pow2_23 =  8388608.0;
   const float bias = 127.0;
@@ -21,6 +21,40 @@ static inline Vec4f vec_pow2n (Vec4f const & n) {
   Vec4i c = b << 23;
   Vec4f d = reinterpret_f(c);
   return d;
+}
+template<class VTYPE>
+static inline VTYPE vec_inf();
+template<>
+inline __m128 vec_inf() {
+  return _mm_castsi128_ps(_mm_set1_epi32(0x7F800000));
+}
+template<>
+inline __m128d vec_inf() {
+  return _mm_castsi128_pd(_mm_setr_epi32(0,0x7FF00000,0,0x7FF00000));
+}
+template<>
+inline __m256 vec_inf() {
+  return constant8f<0x7F800000,0x7F800000,0x7F800000,0x7F800000,0x7F800000,0x7F800000,0x7F800000,0x7F800000>();
+}
+template<>
+inline __m256d vec_inf() {
+  return _mm256_castps_pd(constant8f<0,0x7FF00000,0,0x7FF00000,0,0x7FF00000,0,0x7FF00000>());
+}
+template<>
+inline __m512 vec_inf() {
+  union {
+    int32_t i;
+    float f;
+  } u = {0x7F800000};
+  return _mm512_set1_ps(u.f);
+}
+template<>
+inline __m512d vec_inf() {
+  union {
+    uint64_t i;
+    double f;
+  } u = {0x7FF0000000000000};
+  return _mm512_set1_pd(u.f);
 }
 
 #if __AVX__
@@ -33,7 +67,6 @@ static inline Vec4d vec_pow2n (Vec4d const & n) {
   Vec4d d = reinterpret_d(c);
   return d;
 }
-
 static inline Vec8f vec_pow2n (Vec8f const & n) {
   const float pow2_23 =  8388608.0;
   const float bias = 127.0;
@@ -55,7 +88,6 @@ static inline Vec8d vec_pow2n (Vec8d const & n) {
   Vec8d d = Vec8d(reinterpret_d(c));
   return d;
 }
-
 static inline Vec16f vec_pow2n (Vec16f const & n) {
   const float pow2_23 =  8388608.0;
   const float bias = 127.0;
@@ -69,12 +101,13 @@ static inline Vec16f vec_pow2n (Vec16f const & n) {
 
 template<class VTYPE, class VTYPE2, class BVTYPE, class BVTYPE2>
 static inline VTYPE exp_f(VTYPE2 const & initial_x2) {
-  const float P0expf = 1.f/2.f;
-  const float P1expf = 1.f/6.f;
-  const float P2expf = 1.f/24.f;
-  const float P3expf = 1.f/120.f;
-  const float P4expf = 1.f/720.f;
-  const float P5expf = 1.f/5040.f;
+  const float p2 = 1.f/2.f;
+  const float p3 = 1.f/6.f;
+  const float p4 = 1.f/24.f;
+  const float p5 = 1.f/120.f;
+  const float p6 = 1.f/720.f;
+  const float p7 = 1.f/5040.f;
+  const float log2e = 1.44269504088896340736;
   VTYPE2  x, r, x2, z, n2;
   VTYPE initial_x = initial_x2;
   BVTYPE2 inrange2;
@@ -85,12 +118,12 @@ static inline VTYPE exp_f(VTYPE2 const & initial_x2) {
   const float ln2f_lo  = -2.12194440e-4f;
   max_x = 87.3f;
   x = initial_x;
-  r = round(initial_x*float(VM_LOG2E));
+  r = round(initial_x * log2e);
   x = nmul_add(r, VTYPE2(ln2f_hi), x);
   x = nmul_add(r, VTYPE2(ln2f_lo), x);
 
   x2 = x * x;
-  z = polynomial_5(x,P0expf,P1expf,P2expf,P3expf,P4expf,P5expf);
+  z = polynomial_5(x,p2,p3,p4,p5,p6,p7);
   z = mul_add(z, x2, x);
   n2 = vec_pow2n(r);
   z = (z + 1.0f) * n2;
@@ -99,7 +132,7 @@ static inline VTYPE exp_f(VTYPE2 const & initial_x2) {
   if (horizontal_and(inrange)) {
     return z;
   } else {
-    r = select(sign_bit(initial_x2), 0.f, infinite_vec<VTYPE2>());
+    r = select(sign_bit(initial_x2), 0.f, vec_inf<VTYPE>());
     z = select(inrange2, z, r);
     z = select(is_nan(initial_x2), initial_x2, z);
     return z;
@@ -134,16 +167,16 @@ static inline VTYPE exp_d(VTYPE2 const & initial_x2) {
   const double p11 = 1./39916800.;
   const double p12 = 1./479001600.;
   const double p13 = 1./6227020800.;
-  double max_x;
+  const double max_x = 708.39;
+  const double log2e = 1.44269504088896340736;
+  const double ln2d_hi = 0.693145751953125;
+  const double ln2d_lo = 1.42860682030941723212E-6;
   VTYPE2  x, r, z, n2;
   VTYPE initial_x = initial_x2;
   BVTYPE inrange;
 
-  max_x = 708.39;
-  const double ln2d_hi = 0.693145751953125;
-  const double ln2d_lo = 1.42860682030941723212E-6;
   x  = initial_x2;
-  r  = round(initial_x2*VM_LOG2E);
+  r  = round(initial_x2*log2e);
   x = nmul_add(r, ln2d_hi, x);
   x = nmul_add(r, ln2d_lo, x);
 
