5666a30a7402e548b99161adcdb9f342ef53aa76
Mustafa Abdul Jabbar <mustafa.abduljabbar@kaust.edu.sa>
2017-08-23 14:14:11 +0300

    only used build_tree_tbb.h

--- include/build_tree.h
+++ include/build_tree.h
@@ -2,21 +2,22 @@
 #define build_tree_h
 
 #include "config.h"
-
-#ifdef EXAFMM_WITH_CILK
-
-#if defined EXAFMM_WITH_TBB || defined EXAFMM_WITH_MTHREAD || defined EXAFMM_WITH_QTHREAD
-#include "build_tree_cilk.h"
-#else
-#include "build_tree_tbb.h"
-#endif
-
-#elif defined EXAFMM_WITH_TBB || defined EXAFMM_WITH_MTHREAD || defined EXAFMM_WITH_QTHREAD
 #include "build_tree_tbb.h"
 
-#else
-#include "build_tree_omp.h"
+// #ifdef EXAFMM_WITH_CILK
 
-#endif
+// #if defined EXAFMM_WITH_TBB || defined EXAFMM_WITH_MTHREAD || defined EXAFMM_WITH_QTHREAD
+// #include "build_tree_cilk.h"
+// #else
+// #include "build_tree_tbb.h"
+// #endif
+
+// #elif defined EXAFMM_WITH_TBB || defined EXAFMM_WITH_MTHREAD || defined EXAFMM_WITH_QTHREAD
+// #include "build_tree_tbb.h"
+
+// #else
+// #include "build_tree_omp.h"
+
+// #endif
 
 #endif
